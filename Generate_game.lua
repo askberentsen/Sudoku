c7cfa37248d14852ac29299e44ad9b9565a0de5b
Ask Hetland Berentsen <askberentsen@gmail.com>
2012-12-16 00:36:20 +0100

    Generation more randomized
    -generation process is system dependant
    -generation process is more randomized and will in most cases give a
    unique game
    Feedback is apreciated.

--- Generate_game.lua
+++ Generate_game.lua
@@ -1,11 +1,19 @@
 require "insert"
 function generate_game()
-	if check_board() then
-		for c = 1,(clock_random()*1000) do
-			percentage = 89-(61+(math.random(20))) --7 -> 27
+	if check_board(true) then
+		local seed_generate = seed()
+		local seed_percentage = 0
+		if seed_percentage > 20 then
+			while seed_percentage > 20 do
+				seed_percentage = seed_percentage/10
+			end
 		end
-		percentage_original = percentage
-		generate = 81 - percentage
+		for c = 1,(seed()/1000) do
+			percentage = 93-(61+(math.random(20))) --7 -> 27
+		end
+		local percentage_original = percentage
+		local generate = 81 - percentage
+		local update_percentage = 0
 		while generate > 0 do
 			if generate ~= update_percentage then
 				os.execute("cls")
@@ -15,7 +23,7 @@ function generate_game()
 			compare_index()
 			for a = 1,9 do
 				for b = 1,9 do
-					if math.random(81) <= generate and (index_validate[a][b] == 1 or index_validate[a][b] == "1") and math.random(83) == 1 then
+					if math.random(81) <= generate and (index_validate[a][b] == 1 or index_validate[a][b] == "1") and math.random(seed_generate) <= 101 then
 						generate = generate - 1
 						x[a][b] = 0
 					end
@@ -25,12 +33,22 @@ function generate_game()
 		print()
 	end
 end
-function clock_random()
+function seed()
+	local t1 = os.clock()
 	local out = 0
-	if os.clock() >= 1 then
-		out = os.clock()-math.floor(os.clock())
-	else
-		out = os.clock()
+	while true do
+		if os.clock() - t1 >= 1/32 then
+			break
+		end
+		out = out + 1
+	end
+	while out > 100000 do
+		out = out / 10
+	end
+	while out < 1000 do
+		out = out * 10
 	end
 	return out
 end
+--seed() is system depentant. Debugging is apreciated.
+--reset_board();generate_game();print_board()
