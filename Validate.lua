7264a90d82e12dd07963a8b5064a665216316a91
Ask Hetland Berentsen <askberentsen@gmail.com>
2012-12-15 20:43:57 +0100

    Minor changes
    check_board() is no longer man validation for the game.
    check_game() is now the main validation for the game.
    -can differentiate between unfinished and faulty game.

--- Validate.lua
+++ Validate.lua
@@ -1,6 +1,6 @@
 require "Unique"
 require "Index"
-function get_box(a,b)
+function get_box(a,b,c)
 	local box_array = {0,0,0,0,0,0,0,0,0}
 	local full_count = 0
 	for count = 0,2 do
@@ -13,16 +13,18 @@ function get_box(a,b)
 		end
 
 	end
-	return unique(box_array)
+	return unique(box_array,c)
 	--a[(x*3)-2][(y*3)-2]
 	--
 end
-function all_boxes()
+function all_boxes(a,b)
 	local out = 0
 	for j = 1,3 do
 		for i = 1,3 do
-			if not get_box(j,i) then
-				io.write("box "..j.."."..i.." is wrong"); print()
+			if not get_box(j,i,a) then
+				if b then
+					io.write("box "..j.."."..i.." is wrong"); print()
+				end
 				out = out + 1
 			end
 		end
@@ -33,24 +35,26 @@ function all_boxes()
 		return true
 	end
 end
-function get_line(a)
+function get_line(a,b)
 	local out = 0
 	if a > 9 then
 		a = a - 9
 		local temp = {x[1][a],x[2][a],x[3][a],x[4][a],x[5][a],x[6][a],x[7][a],x[8][a],x[9][a]}
-		out = unique(temp)
+		out = unique(temp,b)
 	else
-		out = unique(x[a])
+		out = unique(x[a],b)
 	end
 	return out
 end
-function all_lines()
+function all_lines(input1, input2)
 	local out = 0
 	local a = 0
 	while a < 18 do
 		a = a + 1
-		if not get_line(a) then
-			io.write("line "..a.." is wrong"); print()
+		if not get_line(a,input1) then
+			if input2 then
+				io.write("line "..a.." is wrong"); print()
+			end
 			out = out + 1
 		end
 	end
@@ -63,10 +67,28 @@ end
 
 
 
-function check_board()
-	if all_lines() and all_boxes() then
+function check_board(a,b)
+	if all_lines(a,b) and all_boxes(a,b) then
 		return true
 	else
+		return false
+	end
+end
+function check_game(a)
+	if check_board() and not check_board(true) then
+		if a then
+			print("Looks good so far.")
+		end
+		return true
+	elseif check_board() and check_board(true) then
+		if a then
+			print("The game is completed!")
+		end
+		return true
+	else
+		if a then
+			check_board(true,true)
+		end
 		return false
 	end
 end
