37b439257b50f49a86f5e0f85bca15cefafc010f
Ask Hetland Berentsen <askberentsen@gmail.com>
2012-12-16 23:28:10 +0100

    Major update
    Generate_game.lua:
    -function write_index() saves a 9 by 9 array to sudoku_index.txt in text
    format.
    --does not save if array is already in sudoku_index.txt
    -function read_index() translates 9 by 9 arrays from text format saved
    in sudoku_index.txt to a usable array.

--- Generate_game.lua
+++ Generate_game.lua
@@ -1,5 +1,5 @@
 require "insert"
-function generate_game()
+function generate_game(a)
 	if check_board(true) then
 		local seed_generate = seed()
 		local seed_percentage = 0
@@ -16,8 +16,10 @@ function generate_game()
 		local update_percentage = 0
 		while generate > 0 do
 			if generate ~= update_percentage then
-				os.execute("cls")
-				io.write("game generating: "..(100-math.floor((generate/(81-percentage_original))*100)).."%")
+				if a then
+					os.execute("cls")
+					io.write("game generating: "..(100-math.floor((generate/(81-percentage_original))*100)).."%")
+				end
 			end
 			update_percentage = generate
 			compare_index()
@@ -50,5 +52,113 @@ function seed()
 	end
 	return out
 end
---seed() is system depentant. Debugging is apreciated.
---reset_board();generate_game();print_board()
+function write_index(a)
+	local file = io.open("sudoku_index.txt","r")
+	local file_read = file:read()
+	local file_size = file_read:len()
+	local index = index_count(file_size,112)
+	local exist = false
+	for o = 1,index do				--This method is inneficient. Please revisit the code later.
+		local count = 0
+		local index_array = read_index(o)
+		for j = 1,9 do
+			for i = 1,9 do
+				--print(a[j][i],  index_array[j][i])
+				if 0+a[j][i] == force_common(index_array[j][i]) then
+					count = count + 1
+				end
+			end
+		end
+		if count == 81 then
+			exist = true
+			break
+		end
+	end
+	file:close()
+	if not exist then
+		local game = io.open("sudoku_index.txt","a")
+		local new_array = (file_size/112)+1
+		game:write("#",new_array)
+		for j = 1,9 do
+			game:write("&",j,">")
+			for i = 1,9 do
+				game:write(a[j][i])
+			end
+		end
+		game:write("%",new_array)
+		game:close()
+	end
+end
+function force_common(a)
+	out = 0
+	if a =="1" then
+		out = 1
+	elseif a == "2" then
+		out = 2
+	elseif a == "3" then
+		out = 3
+	elseif a == "4" then
+		out = 4
+	elseif a == "5" then
+		out = 5
+	elseif a == "6" then
+		out = 6
+	elseif a == "7" then
+		out = 7
+	elseif a == "8" then
+		out = 8
+	elseif a == "9" then
+		out = 9
+	end
+	return out
+end
+function read_index(a)
+	local file = io.open("sudoku_index.txt")
+	local file_read = file:read()
+	local file_size = file_read:len()
+	local out = false
+	if (file_size%112) == 0 then
+		array_count = index_count(file_size,112)
+		if a > array_count then
+			a = array_count
+		elseif a < 1 then
+			a = 1
+		end
+		local array_pos = file_read:sub(((a-1)*112)+1)
+		local array_full = array_pos:sub(1, 112)
+		local temporary_array = {{0,0,0, 0,0,0, 0,0,0}
+						  ,{0,0,0, 0,0,0, 0,0,0}
+						  ,{0,0,0, 0,0,0, 0,0,0}
+						  ,{0,0,0, 0,0,0, 0,0,0}
+						  ,{0,0,0, 0,0,0, 0,0,0}
+						  ,{0,0,0, 0,0,0, 0,0,0}
+						  ,{0,0,0, 0,0,0, 0,0,0}
+						  ,{0,0,0, 0,0,0, 0,0,0}
+						  ,{0,0,0, 0,0,0, 0,0,0}}
+		for j = 1,9 do
+			local line = ("&"..j..">")
+			local array_line_start = array_full:find(line)+3
+			local array_line_full = array_full:sub(array_line_start,array_line_start+8)
+			for i = 1,9 do
+				local array_field = array_line_full:sub(i,i)
+				temporary_array[j][i] = array_field
+			end
+		end
+		out = temporary_array
+	else
+		print("malformed file")
+	end
+	file:close()
+	return out
+end
+function index_count(a,b)
+	local out = 0
+	while a >= b do
+		out = out + 1
+		a = a - b
+	end
+	return out
+end
+--reset_board()
+--write_index(x)
+--print_board(read_index(1))
